3bfaba6c67f8355224af8e7433ffa19d2f87b652
Roshan Choxi <roshan.choxi@gmail.com>
2018-05-18 20:54:35 -0500

    drawing stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,66 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Pointable from 'react-pointable'
+
 import './App.scss'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      drawing: false,
+      canvas: { width: 0, height: 0 },
+      paths: [],
+      currentPath: [],
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateCanvasDimensions)
+    this.updateCanvasDimensions()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateCanvasDimensions)
+  }
+
+  updateCanvasDimensions() {
+    this.setState({ canvas: { width: window.innerWidth, height: window.innerHeight }})
+  }
+
   pointerDown(event) {
-    console.log('down')
+    const { x, y } = event
+    const ctx = this.canvas.getContext('2d')
+
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+
+    this.setState({drawing: true})
+  }
+
+  pointerMove(event) {
+    if(!this.state.drawing) {
+      return
+    }
+
+    const ctx = this.canvas.getContext('2d')
+    ctx.lineTo(event.x, event.y)
+    ctx.stroke()
+  }
+
+  pointerUp(event) {
+    this.setState({drawing: false})
   }
 
   render() {
     return (
-      <canvas onPointerDown={event => this.pointerDown(event)}>
-      </canvas>
+      <Pointable 
+        onPointerDown={event => this.pointerDown(event)}
+        onPointerMove={event => this.pointerMove(event)}
+        onPointerUp={event => this.pointerUp(event)}
+      >
+        <canvas { ...this.state.canvas } ref={node => this.canvas = node}></canvas>
+      </Pointable>
     )
   }
 }
